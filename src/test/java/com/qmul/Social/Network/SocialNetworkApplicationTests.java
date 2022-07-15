0adf93c8d44f09c1b8aa99faf825563b900529d3
Avinash B <a4avinash@hotmail.com>
2022-07-15 11:52:59 +0100

    first commit

---
+++ src/test/java/com/qmul/Social/Network/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmul.Social.Network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
